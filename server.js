a6ea7e537984b0bb166dbe595f01fc3f51a9d670
SamuelVera <samuelvera1999@gmail.com>
2019-06-28 01:11:33 -0400

    MA

--- server.js
+++ server.js
@@ -74,9 +74,9 @@ sequelize
   });
 
     // Set port to be used by Node.js
-  app.set('port', process.env.PORT || 5000)
+    const port = process.env.PORT || 5000
 
-  app.listen(app.get('port'), () => {
-      console.log('Node app is running on port', app.get('port'))
+  app.listen(port, () => {
+      console.log('Node app is running on port', port)
   })
 
